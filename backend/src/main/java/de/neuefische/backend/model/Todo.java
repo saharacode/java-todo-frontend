1cf751af8a6d659e3279ba123251bc42c048f51f
saharacode <johanndallmann@gmx.de>
2023-05-05 11:58:07 +0200

    status is changed from String to Enum

--- backend/src/main/java/de/neuefische/backend/model/Todo.java
+++ backend/src/main/java/de/neuefische/backend/model/Todo.java
@@ -10,16 +10,16 @@ import lombok.RequiredArgsConstructor;
 public class Todo {
     private String id;
     private String description;
-    private String status;
+    private TodoStatus status;
 
     // two constructors needed:
-    public Todo(String id, String description, String status) { // building the object with id
+    public Todo(String id, String description, TodoStatus status) { // building the object with id
         this.id = id;
         this.description = description;
         this.status = status;
     }
 
-    public Todo(String description, String status) { // build the object with the input from the frontend
+    public Todo(String description, TodoStatus status) { // build the object with the input from the frontend
         this.description = description;
         this.status = status;
     }
